476de94699274b0506e3ca857f4947a7f44c5668
Charles Lemmert <81480730+Astro-Alien@users.noreply.github.com>
2022-08-18 18:12:00 +0200

    current update

--- components/booking-component/booking-component.js
+++ components/booking-component/booking-component.js
@@ -3,7 +3,11 @@
      async connectedCallback() {
          this.innerHTML = await fetch(
              import.meta.url.replace(".js", ".html")).then(result => result.text());
-
+         this.objectElement = {
+             firstName: "John",
+             lastName: "Doe",
+             age: 20
+         }
          requestAnimationFrame(() => {
              this.updateBookingMessageHandler = this._updateBookingMessage.bind(this);
              this.addEventListener("change", this.updateBookingMessageHandler);
@@ -13,12 +17,19 @@
      async disconnectedCallback() {
          this.removeEventListener("change", this.updateBookingMessageHandler);
          this.updateBookingMessageHandler = null;
+         this.objectElement = null;
+         this.elementType =null;
+         this.messageElement = null;
+
 
      }
 
      _updateBookingMessage(event) {
-
-
+        this.elementType = event.target.dataset.value;
+        this.messageElement = this.querySelector("#message");
+        this.objectElement[this.elementType] = event.target.value;
+        this.messageElement.textContent = "Booking for " + this.objectElement.firstName + " "+
+            this.objectElement.lastName;
 
      }
 
