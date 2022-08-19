7480211bc76d49d6521ade61d4600c1750b5ceab
Charles Lemmert <81480730+Astro-Alien@users.noreply.github.com>
2022-08-19 09:45:56 +0200

    update to previous code

--- components/booking-component/booking-component.js
+++ components/booking-component/booking-component.js
@@ -3,11 +3,10 @@
      async connectedCallback() {
          this.innerHTML = await fetch(
              import.meta.url.replace(".js", ".html")).then(result => result.text());
-         this.objectElement = {
-             firstName: "John",
-             lastName: "Doe",
-             age: 20
-         }
+
+         this.messageElement = this.querySelector("#message");
+
+
          requestAnimationFrame(() => {
              this.updateBookingMessageHandler = this._updateBookingMessage.bind(this);
              this.addEventListener("change", this.updateBookingMessageHandler);
@@ -17,22 +16,22 @@
      async disconnectedCallback() {
          this.removeEventListener("change", this.updateBookingMessageHandler);
          this.updateBookingMessageHandler = null;
-         this.objectElement = null;
-         this.elementType =null;
          this.messageElement = null;
-
-
      }
 
      _updateBookingMessage(event) {
-        this.elementType = event.target.dataset.value;
-        this.messageElement = this.querySelector("#message");
-        this.objectElement[this.elementType] = event.target.value;
-        this.messageElement.textContent = "Booking for " + this.objectElement.firstName + " "+
-            this.objectElement.lastName;
+
+         this[event.target.dataset.id](event);
 
      }
 
+     firstName(event){
+         this.dataset.name = event.target.value;
+     }
+     lastName(event){
+         this.dataset.surname = event.target.value;
+         this.messageElement.textContent = "Booking for "+ this.dataset.name + " " + this.dataset.surname;
+     }
 
 
 
