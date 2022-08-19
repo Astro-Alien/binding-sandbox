495e2011923c1c3352b1b3e5b97a64e6e4bc3c8d
Charles Lemmert <81480730+Astro-Alien@users.noreply.github.com>
2022-08-19 16:43:14 +0200

    Maze-Escape Project
    Still working on selection and video content.

---
+++ app/maze-escape/maze-escape.js
@@ -0,0 +1,6 @@
+import "./../../components/maze-escape-component/maze-escape-component.js";
+export default class MazeEscape extends crsbinding.classes.ViewBase {
+    async connectedCallback() {
+        await super.connectedCallback();
+    }
+}
