1755a6bd0468031e4bb8f0372150d0fd61359f0a
Charles Lemmert <81480730+Astro-Alien@users.noreply.github.com>
2022-08-22 08:18:07 +0200

    Current Update to maze-escape

--- components/maze-escape-component/maze-escape-component.js
+++ components/maze-escape-component/maze-escape-component.js
@@ -3,28 +3,34 @@ class MazeEscapeComponent extends crsbinding.classes.BindableElement{
         return import.meta.url.replace(".js",".html");
     }
     async connectedCallback() {
-       await super.connectedCallback();
-
-       this.mazeElement = this.querySelector(".maze");
-
+        await super.connectedCallback();
+        this.mazeElement = this.querySelector(".maze");
+        this.createMaze(10, 10);
     }
 
     async disconnectedCallback(){
-        this.sizeValue = null;
         this.mazeElement = null;
         this.selectedElementSize = null;
+        this.divElement = null;
     }
 
     mazeSizeChanged(selectedValue){
         this.selectedElementSize = selectedValue.split("x").map(result => Number(result));
-        this.sizeValue = this.selectedElementSize[0];
-        this.createMaze();
+        this.createMaze(this.selectedElementSize[0],this.selectedElementSize[1]);
     }
-    createMaze(){
-        this.dimensions = this.sizeValue * 2;
-        this.mazeElement.style = `width: ${this.dimensions}rem; height:${this.dimensions}rem`;
-        this.mazeElement.style.gridTemplate = `repeat(${this.sizeValue},2rem)/repeat(${this.sizeValue},2rem)`;
+    createMaze(gridRows,gridColumns){
+        this.mazeElement.innerHTML = "";
+        this.mazeElement.style.gridTemplate = `repeat(${gridRows},2rem)/repeat(${gridColumns},2rem)`;
+
+        for(let i = 0; i <gridRows * gridColumns; i++){
+               this.divElement = document.createElement("div");
+               this.divElement.classList.add("gap");
+
+               this.mazeElement.appendChild(this.divElement);
+
+        }
     }
+
 
 
 }
