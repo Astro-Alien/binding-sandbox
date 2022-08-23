d1fc4b3e7cc341658f0d5b4cba588ecc1270cde4
Charles Lemmert <81480730+Astro-Alien@users.noreply.github.com>
2022-08-23 16:57:12 +0200

    current update for maze project

--- app/booking/booking.js
+++ app/booking/booking.js
@@ -1,5 +1,4 @@
 import "./../../components/booking-component/booking-component.js";
-
 export default class Welcome extends crsbinding.classes.ViewBase {
     async connectedCallback() {
         await super.connectedCallback();
--- components/maze-escape-component/maze-escape-component.js
+++ components/maze-escape-component/maze-escape-component.js
@@ -5,33 +5,32 @@ class MazeEscapeComponent extends crsbinding.classes.BindableElement{
     async connectedCallback() {
         await super.connectedCallback();
         this.mazeElement = this.querySelector(".maze");
-        this.createMaze(10, 10);
+        this._createMaze(10, 10);
     }
 
     async disconnectedCallback(){
         this.mazeElement = null;
         this.selectedElementSize = null;
         this.divElement = null;
+
     }
 
     mazeSizeChanged(selectedValue){
         this.selectedElementSize = selectedValue.split("x").map(result => Number(result));
-        this.createMaze(this.selectedElementSize[0],this.selectedElementSize[1]);
+        this._createMaze(this.selectedElementSize[0],this.selectedElementSize[1]);
     }
-    createMaze(gridRows,gridColumns){
+    _createMaze(gridRows,gridColumns){
         this.mazeElement.innerHTML = "";
         this.mazeElement.style.gridTemplate = `repeat(${gridRows},2rem)/repeat(${gridColumns},2rem)`;
 
+        const fragment = document.createDocumentFragment();
         for(let i = 0; i <gridRows * gridColumns; i++){
                this.divElement = document.createElement("div");
                this.divElement.classList.add("gap");
-
-               this.mazeElement.appendChild(this.divElement);
-
+               fragment.appendChild(this.divElement);
         }
+        this.mazeElement.appendChild(fragment);
     }
-
-
-
+    
 }
 customElements.define("maze-escape-component", MazeEscapeComponent);
