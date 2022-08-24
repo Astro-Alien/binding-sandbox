88c796d13d176606f01c2e0c26ab10a4ca1221ca
Charles Lemmert <81480730+Astro-Alien@users.noreply.github.com>
2022-08-24 12:18:13 +0200

    Current Update to maze-escape
    added stopPropagation method to prevent event bubbling.

--- components/maze-escape-component/maze-escape-component.js
+++ components/maze-escape-component/maze-escape-component.js
@@ -29,6 +29,8 @@ class MazeEscapeComponent extends crsbinding.classes.BindableElement{
         this._createMaze(this.selectedElementSize[0],this.selectedElementSize[1]);
     }
     _createMaze(gridRows,gridColumns){
+        this.divSize = gridRows * 2;
+        this.mazeElement.style = `width:${this.divSize}rem; height${this.divSize}rem`;
         this.mazeElement.innerHTML = "";
         this.mazeElement.style.gridTemplate = `repeat(${gridRows},2rem)/repeat(${gridColumns},2rem)`;
 
@@ -45,13 +47,17 @@ class MazeEscapeComponent extends crsbinding.classes.BindableElement{
             event.target.style.background = "black";
             this.addEventListener('mouseover',this.mouseOverHandler);
             this.addEventListener('mouseup',this.mouseUpHandler);
+            event.stopPropagation();
     }
     mouseOver(event){
              event.target.style.background = "black";
+             event.stopPropagation();
+
     }
     mouseUp(event){
         this.removeEventListener('mouseover',this.mouseOverHandler);
         this.removeEventListener('mouseup',this.mouseUpHandler);
+        event.stopPropagation();
     }
 
 }
